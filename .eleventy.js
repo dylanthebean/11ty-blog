916497f0d22b9c33d17883cb187ec28ea24f6f7f
dylanthebean <dylan.behan93@gmail.com>
2021-12-01 23:32:22 +0000

    add metagen

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const metagen = require("eleventy-plugin-metagen");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(metagen);
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
